f9512e60104044b406b24eb858d4eed87c1f8c01
Hrithik Shah <hrithikchamp@gmail.com>
2018-12-21 02:04:01 -0500

    Changes
    Just made some changes to these files.

--- pizzapy/customer.py
+++ pizzapy/customer.py
@@ -3,12 +3,11 @@ from .address import Address
 class Customer:
     """The Customer who orders a pizza."""
 
-    def __init__(self, fname='', lname='', email='', phone='', address=None):
+    def __init__(self, fname='', lname='', email='', phone=''):
         self.first_name = fname.strip()
         self.last_name = lname.strip()
         self.email = email.strip()
         self.phone = str(phone).strip()
-        self.address = Address(*address.split(','))
 
     def __repr__(self):
         return "Name: {} {}\nEmail: {}\nPhone: {}\nAddress: {}".format(
@@ -18,3 +17,6 @@ class Customer:
             self.phone,
             self.address,
         )
+
+    def add_Address(self, address):
+        self.address = address
